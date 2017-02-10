cf45facfaa009d731580b0c0d3c5393072e78403
julius-t <juliustrojillo@gmail.com>
2017-02-10 15:26:32 -0600

    added question page skeleton

---
+++ AndroidApp/MyApplication/app/src/main/java/comp4350groupa/triviasmack/QuestionPageActivity.java
@@ -0,0 +1,13 @@
+package comp4350groupa.triviasmack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuestionPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question_page);
+    }
+}
