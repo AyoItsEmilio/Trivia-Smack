5d54382acff83af4abbce743b0740c9c2f29ebea
julius-t <juliustrojillo@gmail.com>
2017-02-22 13:06:44 -0600

    linked all the pages together with the corresponding buttons accordingly. Also styled the quiz page

--- AndroidApp/MyApplication/app/src/main/java/comp4350groupa/triviasmack/HomePage.java
+++ AndroidApp/MyApplication/app/src/main/java/comp4350groupa/triviasmack/HomePage.java
@@ -1,7 +1,10 @@
 package comp4350groupa.triviasmack;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomePage extends AppCompatActivity {
 
@@ -9,5 +12,38 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        Button quickPlayBtn = (Button) findViewById(R.id.quickPlayBtn);
+        Button leaderbdBtn = (Button) findViewById(R.id.leaderboardsBtn);
+        Button userBtn = (Button) findViewById(R.id.usernameBtn);
+        Button settingBtn = (Button) findViewById(R.id.settingsBtn);
+
+        quickPlayBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomePage.this, QuestionPageActivity.class));
+            }
+        });
+
+        leaderbdBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomePage.this, Leaderboard.class));
+            }
+        });
+
+        userBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomePage.this, userPage.class));
+            }
+        });
+
+        settingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomePage.this, Settings.class));
+            }
+        });
     }
 }
