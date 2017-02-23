5c07ae59a1ed9f1499cae8530a4cf7692787bd21
julius-t <juliustrojillo@gmail.com>
2017-02-22 22:54:58 -0600

    styled user page

--- AndroidApp/MyApplication/app/src/main/java/comp4350groupa/triviasmack/Leaderboard.java
+++ AndroidApp/MyApplication/app/src/main/java/comp4350groupa/triviasmack/Leaderboard.java
@@ -1,7 +1,9 @@
 package comp4350groupa.triviasmack;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Leaderboard extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class Leaderboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderboard);
+
+        TextView leaderBoardTxt = (TextView) findViewById(R.id.leaderboardTxt);
+        TextView textView2 = (TextView) findViewById(R.id.textView2);
+        TextView dynScore = (TextView) findViewById(R.id.dynScore);
+
+        leaderBoardTxt.setTypeface(Typeface.createFromAsset(getAssets(), "LuckiestGuy.ttf"));
+        textView2.setTypeface(Typeface.createFromAsset(getAssets(), "LuckiestGuy.ttf"));
+        dynScore.setTypeface(Typeface.createFromAsset(getAssets(), "LuckiestGuy.ttf"));
     }
 }
--- AndroidApp/MyApplication/app/src/main/java/comp4350groupa/triviasmack/userPage.java
+++ AndroidApp/MyApplication/app/src/main/java/comp4350groupa/triviasmack/userPage.java
@@ -1,7 +1,9 @@
 package comp4350groupa.triviasmack;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class userPage extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class userPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_page);
+
+        TextView usernameTxt = (TextView) findViewById(R.id.username);
+        TextView scoreTxt = (TextView) findViewById(R.id.scoreText);
+        TextView leaderboardTxt = (TextView) findViewById(R.id.leaderboardText);
+        TextView dynScore = (TextView) findViewById(R.id.dynamicScore);
+        TextView dynRank = (TextView) findViewById(R.id.dynamicRank);
+
+        usernameTxt.setTypeface(Typeface.createFromAsset(getAssets(), "LuckiestGuy.ttf"));
+        scoreTxt.setTypeface(Typeface.createFromAsset(getAssets(), "LuckiestGuy.ttf"));
+        leaderboardTxt.setTypeface(Typeface.createFromAsset(getAssets(), "LuckiestGuy.ttf"));
+        dynScore.setTypeface(Typeface.createFromAsset(getAssets(), "LuckiestGuy.ttf"));
+        dynRank.setTypeface(Typeface.createFromAsset(getAssets(), "LuckiestGuy.ttf"));
+
     }
 }
