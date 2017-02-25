bccc02378679873faa19e97e344938f2589c814f
jbondoc <bondocj@myumanitoba.ca>
2017-02-24 23:39:47 -0600

    Add id field to Question object

--- web_app/objects/Question.py
+++ web_app/objects/Question.py
@@ -2,11 +2,13 @@
 Question.py
 """
 
+
 class Question(object):
     """For holding a question, it's options, and answer.
        The question will serve as an id"""
 
-    def __init__(self, question, options, answer):
+    def __init__(self, id_, question, options, answer):
+        self.id_ = id_
         self.question = question
         self.options = options
         self.answer = answer
