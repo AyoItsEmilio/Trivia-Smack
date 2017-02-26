0322ef4ce5ac246ab4e61f8979f51692a7c48a54
jbondoc <bondocj@myumanitoba.ca>
2017-02-25 18:41:45 -0600

    Make insert_question in db stub add ids; and change populating the lists to use insert_question

--- web_app/tests/persistence/DataAccessStub.py
+++ web_app/tests/persistence/DataAccessStub.py
@@ -5,41 +5,42 @@ import random
 from web_app.persistence.DataAccessInterface import DataAccessInterface
 from web_app.objects.Question import Question
 
+
 class DataAccessStub(DataAccessInterface):
     """Stub database"""
 
     def __init__(self, name):
         self.db_name = name
         self.questions = []
+        self.curr_id = 0
 
     def open(self):
         """Open the database"""
         print "Opened database"
-        self.questions.append(Question(0, "How much does a male "
-                                       "Polar Bear weigh?",
-                                       ["1200 lbs", "1000 lbs", "600 lbs",
-                                        "Enough to break the ice"], 1))
-        self.questions.append(Question(1, "Is the square root of 10:",
-                                       ["zero", "greater than 3",
-                                        "less than 3"], 1))
-        self.questions.append(Question(2, "Platypuses lay eggs",
-                                       ["true", "false"], 0))
-        self.questions.append(Question(3, "Helsinki is the capitol of:",
-                                       ["Sweden", "Russia", "Finland",
-                                        "Iceland"], 2))
-        self.questions.append(Question(4, "If x+y=3 and 2x+y=4, then x equals",
-                                       ["0", "1", "4", "3"], 1))
-        self.questions.append(Question(5, "If x+y<11 and x>6, then y is:",
-                                       ["positive", "negative",
-                                        "Not determinable"], 2))
-        self.questions.append(Question(6, "The plural of bison is:",
-                                       ["bisons", "buffalo", "bison",
-                                        "buffalos"], 2))
-        self.questions.append(Question(7, "21, 25, 33, 49, 81, ",
-                                       ["162", "113", "144", "145"], 2))
-        self.questions.append(Question(8, "The Balkans are in:",
-                                       ["South America", "Europe", "Australia",
-                                        "Asia"], 1))
+        self.insert_question("How much does a male Polar Bear weigh?",
+                             ["1200 lbs", "1000 lbs", "600 lbs",
+                              "Enough to break the ice"], 1)
+        self.insert_question("Is the square root of 10:",
+                             ["zero", "greater than 3",
+                              "less than 3"], 1)
+        self.insert_question("Platypuses lay eggs",
+                             ["true", "false"], 0)
+        self.insert_question("Helsinki is the capitol of:",
+                             ["Sweden", "Russia", "Finland",
+                              "Iceland"], 2)
+        self.insert_question("If x+y=3 and 2x+y=4, then x equals",
+                             ["0", "1", "4", "3"], 1)
+        self.insert_question("If x+y<11 and x>6, then y is:",
+                             ["positive", "negative",
+                              "Not determinable"], 2)
+        self.insert_question("The plural of bison is:",
+                             ["bisons", "buffalo", "bison",
+                              "buffalos"], 2)
+        self.insert_question("21, 25, 33, 49, 81, ",
+                             ["162", "113", "144", "145"], 2)
+        self.insert_question("The Balkans are in:",
+                             ["South America", "Europe", "Australia",
+                              "Asia"], 1)
 
     def close(self):
         """Close the database"""
@@ -68,9 +69,10 @@ class DataAccessStub(DataAccessInterface):
         """Return the number of questions"""
         return len(self.questions)
 
-    def insert_question(self, _id, question, options, answer):
-        # Will make these so id's are generated, not given as a parameter
-        self.questions.append(Question(_id, question, options, answer))
+    def insert_question(self, question, options, answer):
+        self.questions.append(Question(self.curr_id, question,
+                              options, answer))
+        self.curr_id += 1
 
     def update_question(self, _id, question=None,
                         options=None, answer=None):
