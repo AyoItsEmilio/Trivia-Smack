facc9cd5c2cc59634222b088ec6b136c85b72906
samuel-peers <samuelpeers@gmail.com>
2017-03-03 23:05:24 -0600

    Made it so questions are asked as soon as 2 players join

--- web_app/main/events.py
+++ web_app/main/events.py
@@ -2,16 +2,25 @@
 events.py
 """
 from flask import session
-from flask_socketio import emit
+from flask_socketio import emit, join_room, leave_room
 from .. import socketio
 
 USERS = []
+ROOM = "only_one"
+MAX_PLAYERS = 2
 
 @socketio.on("joined", namespace="/test")
 def joined(message):
-    USERS.append(0)
-    emit("status", {"msg":str(len(USERS))})
+    user_id = len(USERS)+1
+    USERS.append(user_id)
+
+    join_room(ROOM)
+
+    if len(USERS) == MAX_PLAYERS:
+        emit("ready", room=ROOM)
+
 
 @socketio.on("disconnect")
 def disconnect():
+    leave_room(ROOM)
     USERS.pop(-1)
