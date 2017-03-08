8b07a274c51e6d301d3cdfa965b39853a8ba59e9
jbondoc <bondocj@myumanitoba.ca>
2017-03-07 21:49:26 -0600

    Update DataAccessObject Test to create object through Services

--- web_app/tests/persistence/DataAccessObjectTest.py
+++ web_app/tests/persistence/DataAccessObjectTest.py
@@ -1,18 +1,10 @@
 """
 DataAccessObject.py
 """
-import pymongo
-from json import dumps, loads
-from bson import json_util, ObjectId
-from web_app.persistence.DataAccessInterface import DataAccessInterface
-from web_app.objects.Question import Question
+
 from flask.ext.pymongo import MongoClient
 import unittest
-from web_app.persistence.DataAccessObject import DataAccessObject
 from web_app.application.Services import Services
-from web_app.objects.Question import Question
-from web_app.populate_database import populate_db
-
 TEST_DB_NAME = "test"
 
 
@@ -24,26 +16,25 @@ class DataAccessObjectTest(unittest.TestCase):
     python -m unittest web_app.tests.persistence.DataAccessObjectTest
     '''
 
-    @classmethod
-    def setUpClass(cls):
-        """Call once"""
-        cls.obj = DataAccessObject(TEST_DB_NAME)
-        cls.obj.open()
-        cls.client = MongoClient()
-        cls.mongo = cls.client[TEST_DB_NAME]
-
     def setUp(self):
-        pass
+        Services.create_data_access(
+            dbName=TEST_DB_NAME)
+        self.data_access = Services.get_data_access()
+
+        self.client = MongoClient()
+        self.mongo = self.client[TEST_DB_NAME]
 
     def tearDown(self):
-        pass
+        """Call once"""
+        Services.close_data_access()
+        self.client.drop_database(TEST_DB_NAME)
 
     def test_insert_question(self):
         print 'testing insert question'
         question = "How many breeds of cats exist?"
         options = [16, 36, 74, 90]
         answer = 2
-        question_id = self.obj.insert_question(
+        question_id = self.data_access.insert_question(
             question=question,
             options=options,
             answer=answer
@@ -55,9 +46,3 @@ class DataAccessObjectTest(unittest.TestCase):
         self.assertEqual(question, inserted_question['question'])
         self.assertEqual(options, inserted_question['options'])
         self.assertEqual(answer, inserted_question['answer'])
-
-    @classmethod
-    def tearDownClass(cls):
-        """Call once"""
-        Services.close_data_access()
-        cls.client.drop_database(TEST_DB_NAME)
