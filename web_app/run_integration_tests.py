0d0bfcc2ac6435b4ebbf67acc5987f5d9e2265b9
samuel-peers <samuelpeers@gmail.com>
2017-03-11 16:11:36 -0600

    Wrote integration tests for the db

--- web_app/run_integration_tests.py
+++ web_app/run_integration_tests.py
@@ -4,14 +4,18 @@ run_integration_tests.py
 import unittest
 import sys
 from web_app.tests.integration.BusinessPersistenceSeamTest\
-import BusinessPersistenceSeamTest
+    import BusinessPersistenceSeamTest
+from web_app.tests.integration.DataAccessMongoTest import DataAccessMongoTest
 
 def main():
 
     business_seam_suite =\
-    unittest.TestLoader().loadTestsFromTestCase(BusinessPersistenceSeamTest)
+        unittest.TestLoader().loadTestsFromTestCase(BusinessPersistenceSeamTest)
 
-    all_suites = unittest.TestSuite([business_seam_suite])
+    data_access_suite =\
+        unittest.TestLoader().loadTestsFromTestCase(DataAccessMongoTest)
+
+    all_suites = unittest.TestSuite([business_seam_suite, data_access_suite])
 
     runner = unittest.TextTestRunner()
     result = runner.run(all_suites)
--- web_app/tests/persistence/DataAccessTest.py
+++ web_app/tests/persistence/DataAccessTest.py
@@ -7,47 +7,53 @@ from web_app.application.Services import Services
 from web_app.objects.Question import Question
 
 class DataAccessTest(unittest.TestCase):
-    """Unit tests for the DataAccess class"""
-
-    @classmethod
-    def setUpClass(cls):
-        Services.close_data_access()
-        Services.create_data_access(\
-            altDataAccessService=DataAccessStub("application"))
+    """Tests for the DataAccess class"""
 
     @classmethod
     def tearDownClass(cls):
         Services.close_data_access()
 
-    def setUp(self):
-        self.data_access = Services.get_data_access()
-        self.db_size = 9
-
-    def tearDown(self):
-        self.data_access = None
-
-    def test_get_question(self):
+    @classmethod
+    def data_access_test(cls, self):
+        
         print "Testing DataAccess: get_question"
 
-        question_obj = self.data_access.get_question()
+        data_access = Services.get_data_access()
+
+        question_obj = data_access.get_question()
 
         self.assertIsInstance(question_obj, Question)
         self.assertIsNotNone(question_obj)
 
-    def test_get_all_questions(self):
         print "Testing DataAccess: get_all_questions"
 
-        all_questions = self.data_access.get_all_questions()
+        db_size = 9
+
+        data_access = Services.get_data_access()
+
+        all_questions = data_access.get_all_questions()
 
         self.assertIsInstance(all_questions, list)
-        self.assertEquals(len(all_questions), self.db_size)
+        self.assertEquals(len(all_questions), db_size)
         self.assertEquals(all_questions[0].question,\
                           "How much does a male Polar Bear weigh?")
 
-    def test_get_num_questions(self):
         print "Testing DataAccess: get_num_questions"
 
-        num_questions = self.data_access.get_num_questions()
+        db_size = 9
+
+        data_access = Services.get_data_access()
+
+        num_questions = data_access.get_num_questions()
 
         self.assertIsInstance(num_questions, int)
-        self.assertEquals(self.db_size, num_questions)
+        self.assertEquals(db_size, num_questions)
+
+    def test_data_access(self):
+        print "Testing DataAccess using db stub"
+
+        Services.close_data_access()
+        Services.create_data_access(\
+            altDataAccessService=DataAccessStub("application"))
+
+        DataAccessTest.data_access_test(self)
