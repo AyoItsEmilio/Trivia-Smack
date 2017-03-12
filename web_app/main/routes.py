fbcd404d679406e52cd35a63b9fafa68881bdf04
samuel-peers <samuelpeers@gmail.com>
2017-03-12 16:57:50 -0600

    Just set the route method to get

--- web_app/main/routes.py
+++ web_app/main/routes.py
@@ -40,12 +40,12 @@ def question_page():
        options=options,\
        answer=answer)
 
-@main.route("/api/android/question_data/<num_questions>")
+@main.route("/api/question_data/<num_questions>", methods=["GET"])
 def question_data(num_questions):
     access_questions = AccessQuestions()
     questions = access_questions.get_random_questions(int(num_questions))
 
-    return jsonify(result=questions)
+    return jsonify(questions=questions)
 
 @main.route("/api/android/post_score", methods=["POST"])
 def post_score():
