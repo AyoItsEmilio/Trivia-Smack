5f2c6a92df96b0440f2cc75bf89e87de49000da7
samuel-peers <samuelpeers@gmail.com>
2017-03-12 08:44:49 -0600

    Wrote integration tests for data access

--- web_app/tests/persistence/DataAccessTest.py
+++ web_app/tests/persistence/DataAccessTest.py
@@ -10,149 +10,195 @@ class DataAccessTest(unittest.TestCase):
 
     @classmethod
     def data_access_test(cls, self):
-        pass
 
-    def setUp(self):
-        Services.create_data_access(
-            altDataAccessService=DataAccessStub("application"))
-        self.data_access = Services.get_data_access()
-        self.db_size = 9
+        DataAccessTest.data_access = Services.get_data_access()
+        DataAccessTest.db_size = 9
 
-    def tearDown(self):
+        DataAccessTest.get_question_test(self)
+        DataAccessTest.get_random_question_test(self)
+        DataAccessTest.get_all_questions_test(self)
+        DataAccessTest.get_num_questions_test(self)
+        DataAccessTest.update_question_test(self)
+        DataAccessTest.update_options_test(self)
+        DataAccessTest.update_answer_test(self)
+        DataAccessTest.delete_question_test(self)
+        DataAccessTest.update_all_test(self)
+        DataAccessTest.update_answer_test(self)
+
         Services.close_data_access()
         self.data_access = None
 
-    def test_get_question(self):
+    @classmethod
+    def get_question_test(cls, self):
         print "Testing DataAccess: get_question"
 
-        target_question = Question(2, "Platypuses lay eggs",
-                                   ["true", "false"], 0)
+        question = "Platypuses lay eggs"
+        target_question = Question(question, ["true", "false"], 0)
 
-        question_obj = self.data_access.get_question(_id=2)
-        self.assertEquals(target_question._id, question_obj._id)
+        question_obj = DataAccessTest.data_access.get_question(question=question)
+
         self.assertEquals(target_question.question, question_obj.question)
         self.assertEquals(target_question.options, question_obj.options)
         self.assertEquals(target_question.answer, question_obj.answer)
 
-    def test_get_random_question(self):
+    @classmethod
+    def get_random_question_test(cls, self):
         print "Testing DataAccess: get_random_question"
 
-        question_obj = self.data_access.get_random_question()
+        question_obj = DataAccessTest.data_access.get_random_question()
 
         self.assertIsInstance(question_obj, Question)
         self.assertIsNotNone(question_obj)
 
-    def test_get_all_questions(self):
+    @classmethod
+    def get_all_questions_test(cls, self):
         print "Testing DataAccess: get_all_questions"
 
-        all_questions = self.data_access.get_all_questions()
+        all_questions = DataAccessTest.data_access.get_all_questions()
 
         self.assertIsInstance(all_questions, list)
-        self.assertEquals(len(all_questions), self.db_size)
+        self.assertEquals(len(all_questions), DataAccessTest.db_size)
         self.assertEquals(all_questions[0].question,
                           "How much does a male Polar Bear weigh?")
 
-    def test_get_num_questions(self):
+    @classmethod
+    def get_num_questions_test(cls, self):
         print "Testing DataAccess: get_num_questions"
 
-        num_questions = self.data_access.get_num_questions()
+        num_questions = DataAccessTest.data_access.get_num_questions()
 
         self.assertIsInstance(num_questions, int)
-        self.assertEquals(self.db_size, num_questions)
+        self.assertEquals(DataAccessTest.db_size, num_questions)
 
-    def test_update_question(self):
+    @classmethod
+    def update_question_test(cls, self):
         print "Testing DataAccess: update_question (Question)"
-        question_id = 8
+
+        old_question = "The Balkans are in:"
         new_question = "The Balkans are located in:"
         options = ["South America", "Europe", "Australia", "Asia"]
         answer = 1
 
-        self.data_access.update_question(_id=question_id,
-                                         new_question=new_question)
+        result =\
+        DataAccessTest.data_access.update_question(old_question=old_question,
+                                                   new_question=new_question)
 
-        question_object = self.data_access.get_question(_id=8)
+        self.assertIsNotNone(result)
+
+        question_object =\
+            DataAccessTest.data_access.get_question(question=new_question)
 
         self.assertEquals(question_object.question, new_question)
         self.assertEquals(question_object.options, options)
         self.assertEquals(question_object.answer, answer)
 
-    def test_update_options(self):
+        DataAccessTest.data_access.update_question(old_question=new_question,
+                                                   new_question=old_question)
+
+    @classmethod
+    def update_options_test(cls, self):
         print "Testing DataAccess: update_question (Options)"
-        question_id = 8
+
         question = "The Balkans are in:"
-        new_options = ["South America", "Europe", "Australia",
-                       "Africa"]
+        old_options = ["South America", "Europe", "Australia", "Asia"]
+        new_options = ["South America", "Europe", "Australia", "Africa"]
         answer = 1
 
-        self.data_access.update_question(_id=question_id, new_options=new_options)
+        result =\
+        DataAccessTest.data_access.update_question(old_question=question,
+                                                   new_options=new_options)
 
-        question_object = self.data_access.get_question(_id=8)
+        self.assertIsNotNone(result)
+
+        question_object =\
+        DataAccessTest.data_access.get_question(question=question)
 
         self.assertEquals(question_object.question, question)
         self.assertEquals(question_object.options, new_options)
         self.assertEquals(question_object.answer, answer)
 
-    def test_update_answer(self):
+        DataAccessTest.data_access.update_question(old_question=question,
+                                                   new_options=old_options)
+
+    @classmethod
+    def update_answer_test(cls, self):
         print "Testing DataAccess: update_question (Answer)"
-        question_id = 8
+
         question = "The Balkans are in:"
         options = ["South America", "Europe", "Australia", "Asia"]
+        old_answer = 1
         new_answer = 0
 
-        self.data_access.update_question(_id=question_id, new_answer=new_answer)
+        DataAccessTest.data_access.update_question(old_question=question,\
+                                                   new_answer=new_answer)
 
-        question_object = self.data_access.get_question(_id=8)
+        question_object =\
+        DataAccessTest.data_access.get_question(question=question)
 
         self.assertEquals(question_object.question, question)
         self.assertEquals(question_object.options, options)
         self.assertEquals(question_object.answer, new_answer)
 
-    def test_update_answer2(self):
-        print "Testing DataAccess: update_question (Answer)"
+        DataAccessTest.data_access.update_question(old_question=question,
+                                                   new_answer=old_answer)
 
-        question = "The Balkans are in:"
-        options = ["South America", "Europe", "Australia", "Asia"]
-        new_answer = 0
-
-        self.data_access.update_question(question=question, new_answer=new_answer)
-
-        question_object = self.data_access.get_question(_id=8)
-
-        self.assertEquals(question_object.question, question)
-        self.assertEquals(question_object.options, options)
-        self.assertEquals(question_object.answer, new_answer)
-
-    def test_update_all(self):
+    @classmethod
+    def update_all_test(cls, self):
         print "Testing DataAccess: update_question (All)"
 
-        question_id = 8
         new_question = "How many breeds of cats are there"
-        new_options = [35, 58, 73, 112]
+        new_options = ["35", "58", "73", "112"]
         new_answer = 2
 
-        self.data_access.update_question(_id=question_id,
-                                         new_question=new_question,
-                                         new_options=new_options,
-                                         new_answer=new_answer)
+        old_question = "The Balkans are in:"
+        old_options = ["South America", "Europe", "Australia", "Asia"]
+        old_answer = 1
 
-        question_object = self.data_access.get_question(_id=8)
+        DataAccessTest.data_access.update_question(old_question=old_question,
+                                                   new_question=new_question,
+                                                   new_options=new_options,
+                                                   new_answer=new_answer)
+
+        question_object =\
+        DataAccessTest.data_access.get_question(question=new_question)
 
         self.assertEquals(question_object.question, new_question)
         self.assertEquals(question_object.options, new_options)
         self.assertEquals(question_object.answer, new_answer)
 
-    def test_delete_question(self):
-        self.data_access.delete_question(5)
+        DataAccessTest.data_access.update_question(old_question=new_question,
+                                                   new_question=old_question,
+                                                   new_options=old_options,
+                                                   new_answer=old_answer)
 
-        all_questions = self.data_access.get_all_questions()
+    @classmethod
+    def delete_question_test(cls, self):
+        print "Testing DataAccess: delete_question"
 
-        self.assertEquals(len(all_questions), self.db_size-1)
+        question = "The Balkans are in:"
+        question_obj =\
+        DataAccessTest.data_access.get_question(question=question)
+
+        DataAccessTest.data_access.delete_question(question=question)
+
+        all_questions = DataAccessTest.data_access.get_all_questions()
+
+        self.assertEquals(len(all_questions), (DataAccessTest.db_size-1))
+
+        question_none =\
+        DataAccessTest.data_access.get_question(question=question)
+
+        self.assertIsNone(question_none)
+
+        DataAccessTest.data_access.insert_question(question_obj.question,
+                                                   question_obj.options,
+                                                   question_obj.answer)
 
     def test_data_access(self):
         print "Testing DataAccess using db stub"
 
         Services.close_data_access()
         Services.create_data_access(\
-            altDataAccessService=DataAccessStub("application"))
+            altDataAccessService=DataAccessStub("test_db"))
 
         DataAccessTest.data_access_test(self)
