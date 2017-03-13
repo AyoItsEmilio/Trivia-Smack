0ad2fdeb77bd75469393c0c75ddde29220ae8921
samuel-peers <samuelpeers@gmail.com>
2017-03-13 00:39:28 -0600

    Destroyed useless, ancient routes!

--- web_app/main/routes.py
+++ web_app/main/routes.py
@@ -1,44 +1,9 @@
 """
 routes.py
 """
-from flask import render_template, request, redirect, jsonify
+from flask import jsonify, request
 from web_app.business.AccessQuestions import AccessQuestions
-from web_app.business.GameController import GameController
 from . import main
-
-GAME_CONTROLLER = GameController.get_instance()
-
-@main.route("/")
-def home_page():
-    score = GAME_CONTROLLER.score if GAME_CONTROLLER.is_started else None
-    GAME_CONTROLLER.start()
-
-    return render_template("homePage.html", score=score)
-
-@main.route("/question_page", methods=["GET", "POST"])
-def question_page():
-
-    if not GAME_CONTROLLER.is_started:
-        GAME_CONTROLLER.start()
-
-    result = request.args.get("result", None)
-
-    if result is not None and GAME_CONTROLLER.evaluate_answer(int(result)):
-        GAME_CONTROLLER.increase_score()
-
-    if GAME_CONTROLLER.is_finished():
-        return redirect("/")
-
-    question_obj = GAME_CONTROLLER.get_next_question()
-
-    question = question_obj.question
-    options = question_obj.options
-    answer = question_obj.answer
-
-    return render_template("questionPage.html",\
-       question=question,\
-       options=options,\
-       answer=answer)
 
 @main.route("/api/question_data/<num_questions>", methods=["GET"])
 def question_data(num_questions):
