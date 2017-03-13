1abb95e7bef325bd560035ec6cb75a702ba5f580
Thomas Beckett <beckettt@myumanitoba.ca>
2017-03-13 01:13:49 -0500

    Updated the UI of the username page. Name updates after you enter a new one, and a dialog pops up it the name exceeds 15 characters.

--- android_app/app/src/main/java/comp4350/triviasmack/presentation/UsernameSelectActivity.java
+++ android_app/app/src/main/java/comp4350/triviasmack/presentation/UsernameSelectActivity.java
@@ -1,7 +1,9 @@
 package comp4350.triviasmack.presentation;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.View;
@@ -17,7 +19,7 @@ import comp4350.triviasmack.objects.Profile;
 public class UsernameSelectActivity extends AppCompatActivity {
 
     private GameController gameController;
-    private final int MAX_CHARACTERS = 21;
+    private final static int maxCharacters = 15;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,13 +36,20 @@ public class UsernameSelectActivity extends AppCompatActivity {
                 boolean handled = false;
                 if(i == EditorInfo.IME_ACTION_DONE) {
                     String newUsername = textView.getText().toString();
-                    if(!newUsername.equals("") && newUsername.length() <= MAX_CHARACTERS)
+                    if(!newUsername.equals("") && newUsername.length() <= maxCharacters) {
                         gameController.setNewUsername(newUsername);
+                        setUsernameText();
+                    }
+                    else if(newUsername.length() > maxCharacters)
+                        nameLengthPopup();
                 }
                 return handled;
             }
         });
+        setUsernameText();
+    }
 
+    private void setUsernameText() {
         TextView usernameText = (TextView) findViewById(R.id.usernameText);
         usernameText.setText(gameController.getUsername());
     }
@@ -48,5 +57,22 @@ public class UsernameSelectActivity extends AppCompatActivity {
     public void renderMainPage(View v) {
         Intent MainPageIntent = new Intent(UsernameSelectActivity.this, MainActivity.class);
         UsernameSelectActivity.this.startActivity(MainPageIntent);
+    }
+
+    private void nameLengthPopup() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(UsernameSelectActivity.this);
+        builder.setMessage("Username must be less than "+maxCharacters+" characters.");
+        builder.setCancelable(true);
+
+        builder.setPositiveButton(
+                "OK",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 }
