d4caf0d16cb38ce19d464c42146f4c5d0cac4737
julius-t <trojillj@myumanitoba.ca>
2017-03-21 13:44:24 -0500

    adding init.py

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -22,6 +22,7 @@ def create_app(application):
 def set_up():
     populate_database(DB_NAME)
     Services.create_data_access(dbName=DB_NAME)
+    #Services.create_data_access(altDataAccessService=DataAccessStub("stub"))
 
 def tear_down():
     Services.close_data_access()
