4dc0e926f040faf836f518f44e4607303e7f67d4
samuel-peers <samuelpeers@gmail.com>
2017-03-22 18:47:43 -0500

    Added docker stuff

--- application.py
+++ application.py
@@ -9,12 +9,11 @@ from web_app import create_app, set_up, tear_down, socketio
 set_up()
 
 application = Flask(__name__,\
-    template_folder="web_app/templates",\
     static_folder="web_app/static",\
     static_url_path="")
 
 create_app(application)
 
 if __name__ == "__main__":
-    socketio.run(application)
+    socketio.run(application, host="0.0.0.0")
     tear_down()
