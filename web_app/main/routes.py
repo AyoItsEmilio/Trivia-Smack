5241e8497c4e76c5ef34c90eab549d2637347370
samuel-peers <samuelpeers@gmail.com>
2017-03-25 12:19:41 -0500

    Just removed the post score api

--- web_app/main/routes.py
+++ web_app/main/routes.py
@@ -11,8 +11,3 @@ def question_data(num_questions):
     questions = access_questions.get_random_questions(int(num_questions))
 
     return jsonify(questions=questions)
-
-@main.route("/api/android/post_score", methods=["POST"])
-def post_score():
-    print "DATA: {}".format(request.form["score"])
-    return "Connection succeeded"
