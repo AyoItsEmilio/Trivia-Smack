e42570ab4f67ea1a12f7c94c68ab3c67304cf6f0
samuel-peers <samuelpeers@gmail.com>
2017-03-31 10:00:59 -0500

    Created API routes for adding/getting all questions

--- web_app/main/routes.py
+++ web_app/main/routes.py
@@ -4,7 +4,7 @@ routes.py
 Error handlers are defined at the application level in application.py
 (this is the blueprint level)
 """
-from flask import jsonify, make_response
+from flask import jsonify, make_response, request, abort
 from flask_httpauth import HTTPBasicAuth
 from web_app.business.AccessQuestions import AccessQuestions
 from . import main
@@ -26,9 +26,55 @@ def unauthorized():
 def login():
     return make_response(jsonify(result={"success":True}), 200)
 
+@main.route("/api/add_question", methods=["POST"])
+@auth.login_required
+def add_question():
+    if not request.json or not validate_json(request.json):
+        abort(400)
+
+    access_questions = AccessQuestions()
+
+    question_obj = clean_json(request.json)
+
+    add_result = access_questions.add_question(\
+        question_obj["question"],\
+        question_obj["options"],\
+        question_obj["answer"])
+
+    return make_response(jsonify(result={"success": add_result}), 200)
+
+@main.route("/api/get_questions", methods=["GET"])
+@auth.login_required
+def get_questions():
+    access_questions = AccessQuestions()
+    questions = access_questions.get_random_questions()
+    return make_response(jsonify(questions=questions), 200)
+
 @main.route("/api/question_data/<int:num_questions>", methods=["GET"])
 def question_data(num_questions):
     access_questions = AccessQuestions()
     questions = access_questions.get_random_questions(int(num_questions))
 
     return make_response(jsonify(questions=questions), 200)
+
+def validate_json(json):
+
+    result = True
+
+    try:
+        int(str(json["answer"]))
+    except ValueError:
+        result = False
+
+    result = "question" in json and "options" in json and "answer" in json\
+             and isinstance(json["question"], unicode) and result\
+             and isinstance(json["options"], list) and result
+
+    return result
+
+def clean_json(json):
+    result = {}
+    result["question"] = str(json["question"]).strip()
+    result["options"] = [str(o).strip() for o in json["options"] if o]
+    result["answer"] = int(json["answer"])
+    return result
