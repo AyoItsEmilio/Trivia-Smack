85e73d2dd3318d5cb17e442a2b285bb47c290052
jbondoc <bondocj@myumanitoba.ca>
2017-04-01 19:10:33 -0500

    Add category and difficulty to question object

--- web_app/objects/Question.py
+++ web_app/objects/Question.py
@@ -4,7 +4,9 @@ Question.py
 
 class Question(object):
 
-    def __init__(self, question, options, answer):
+    def __init__(self, question, options, difficulty, category, answer):
         self.question = question
         self.options = options
+        self.difficulty = difficulty
+        self.category = category
         self.answer = answer
