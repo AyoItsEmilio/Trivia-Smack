c1b41070aa55cb9af3a006fe8a027e58847c49c5
jbondoc <bondocj@myumanitoba.ca>
2017-04-09 00:58:03 -0500

    Fix main activity robotium tests

--- android_app/app/src/androidTest/java/comp4350/triviasmack/robotium/MainActivityTest/MainActivityTest.java
+++ android_app/app/src/androidTest/java/comp4350/triviasmack/robotium/MainActivityTest/MainActivityTest.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
 import comp4350.triviasmack.R;
 import comp4350.triviasmack.presentation.MainActivity;
 import comp4350.triviasmack.presentation.MultiPlayerPageActivity;
-import comp4350.triviasmack.presentation.QuestionPageActivity;
+import comp4350.triviasmack.presentation.SelectCategoryActivity;
 
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
 
@@ -40,16 +40,16 @@ public class MainActivityTest {
     public void pressPlayBtn() throws Exception {
         solo.assertCurrentActivity("wrong activity", MainActivity.class);
 
-        solo.clickOnView(solo.getView(R.id.geography));
+        solo.clickOnView(solo.getView(R.id.one_player));
 
-        solo.assertCurrentActivity("wrong activity", QuestionPageActivity.class);
+        solo.assertCurrentActivity("wrong activity", SelectCategoryActivity.class);
     }
 
     @Test
     public void pressTwoPlayerBtn() throws Exception {
         solo.assertCurrentActivity("wrong activity", MainActivity.class);
 
-        solo.clickOnView(solo.getView(R.id.history));
+        solo.clickOnView(solo.getView(R.id.two_player));
 
         solo.assertCurrentActivity("wrong activity", MultiPlayerPageActivity.class);
     }
--- android_app/app/src/main/java/comp4350/triviasmack/application/Constants.java
+++ android_app/app/src/main/java/comp4350/triviasmack/application/Constants.java
@@ -1,5 +1,5 @@
 package comp4350.triviasmack.application;
 
 public class Constants {
-    public static final String SERVER_URL = "http://0.0.0.0:5000/";
+    public static final String SERVER_URL = "http://10.0.2.2:5000/";
 }
