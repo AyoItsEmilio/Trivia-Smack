280224ff2cc8bedab89964d9768f978fe3bde4fc
samuel-peers <samuelpeers@gmail.com>
2017-04-09 20:48:47 -0500

    Removed html-txt file and related methods

--- web_app/static/TaskViewModel.js
+++ web_app/static/TaskViewModel.js
@@ -170,35 +170,16 @@ function TasksViewModel(){
          return self.questions();
      };
 
-     window.onclick = function(event) {
-        if(event.target == modal) {
+    window.onclick = function(event) {
+        if(event.target == modal)
             modal.style.display = "none";
-        }
-    }
+    };
 
     self.closeRules = function() {
         modal.style.display = "none";
-    }
+    };
 
     self.openRules = function() {
-        var rulesText = readRulesFile("trivia_rules.txt");
-        document.getElementById("rulesText").innerHTML=rulesText;
         modal.style.display = "block";
-    }
-
-    function readRulesFile(file) {
-        var text;
-        var rawFile = new XMLHttpRequest();
-        rawFile.open("GET", file, false);
-
-        rawFile.onreadystatechange = function() {
-            if(rawFile.readyState === 4) {
-                if(rawFile.status === 200 || rawFile.status == 0) {
-                    text = rawFile.responseText;
-                }
-            }
-        }
-        rawFile.send(null);
-        return text;
-    }
+    };
 }
