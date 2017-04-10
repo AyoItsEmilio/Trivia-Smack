f893fd6873faa1abf520c0d685a357b8c3f2e4ba
jbondoc <bondocj@myumanitoba.ca>
2017-04-10 15:24:00 -0500

    Add acceptance tests to RunAcceptanceTests

--- android_app/app/src/androidTest/java/comp4350/triviasmack/robotium/RunAcceptanceTests.java
+++ android_app/app/src/androidTest/java/comp4350/triviasmack/robotium/RunAcceptanceTests.java
@@ -4,10 +4,11 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 import comp4350.triviasmack.robotium.MainActivityTest.MainActivityTest;
+import comp4350.triviasmack.robotium.PracticeModeActivityTest.PracticeModeActivityTest;
 import comp4350.triviasmack.robotium.SinglePlayerTest.SinglePlayerTest;
 import comp4350.triviasmack.robotium.TwoPlayerTest.TwoPlayersTest;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({MainActivityTest.class, SinglePlayerTest.class, TwoPlayersTest.class})
+@Suite.SuiteClasses({MainActivityTest.class, SinglePlayerTest.class, TwoPlayersTest.class, PracticeModeActivityTest.class})
 public class RunAcceptanceTests {
 }
