7f4a4766378354c77fae8e1882779564f185f00f
jbondoc <bondocj@myumanitoba.ca>
2017-04-10 14:35:54 -0500

    Make it only fetch 50 questions at a time since it was laggy

--- android_app/app/src/main/java/comp4350/triviasmack/application/Constants.java
+++ android_app/app/src/main/java/comp4350/triviasmack/application/Constants.java
@@ -3,5 +3,4 @@ package comp4350.triviasmack.application;
 public class Constants {
     public static final String SERVER_URL = "http://triviaDockerEnvGOOD.irs2mc4qmz.us-west-2.elasticbeanstalk.com/";
     public static final String[] categories = {"all", "geography", "history", "math and science", "pop culture", "other"};
-    public static final int TOTAL_NUM_QUESTIONS = 410;
 }
